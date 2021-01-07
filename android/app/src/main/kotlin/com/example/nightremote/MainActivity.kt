7ea328cdbc421cbeeea1e8d189ed2d9c1dbb3a51
Gabriele Gigante <gabriele.gigante67@gmail.com>
2021-01-07 18:42:28 +0100

    1.0

--- android/app/src/main/kotlin/com/example/nightremote/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/nightremote/MainActivity.kt
@@ -1,6 +1,27 @@
 package com.example.nightremote
 
+import android.view.KeyEvent
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "volume"
+    private var manager: MethodChannel? = null
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        manager = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL);
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            manager?.invokeMethod("volume", "-");
+        }
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
+            manager?.invokeMethod("volume", "+");
+        }
+        return super.onKeyDown(keyCode, event)
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:nightremote/popup.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 void main() {
@@ -8,11 +13,10 @@ void main() {
 class SocketService {
   IO.Socket socket;
 
-  createSocketConnection() {
-    socket = IO.io('http://192.168.1.4:7000', <String, dynamic>{
+  createSocketConnection(String ip) {
+    socket = IO.io('http://$ip:7000', <String, dynamic>{
       'transports': ['websocket'],
     });
-    print("aaaaaaaaa");
     this.socket.on("connect", (_) => print('Connected'));
     this.socket.on("disconnect", (_) => print('Disconnected'));
   }
@@ -43,6 +47,9 @@ class _MyHomePageState extends State<MyHomePage> {
   SocketService ss = SocketService();
   int count;
   List<double> delta;
+  StreamSubscription vbe;
+  SharedPreferences prefs;
+  String ip;
 
   @override
   void initState() {
@@ -51,36 +58,102 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    initSP();
+    listenToVolume();
     int val = 2;
     count = 0;
     delta = [0, 0];
-    ss.createSocketConnection();
+
     return Scaffold(
-      backgroundColor: Colors.blueGrey[900],
-      body: Container(
-        child: GestureDetector(
-          onTap: () {
-            ss.socket.emit('click');
-            print("boop");
-          },
-          onPanUpdate: (details) {
-            print(details.delta.dx.toString() +
-                " " +
-                details.delta.dy.toString());
-            if (this.count < val) {
-              delta[0] += details.delta.dx;
-              delta[1] += details.delta.dy;
-              count++;
-            }
-            if (count == val) {
-              ss.socket.emit('cursor', [delta[0], delta[1]]);
-              delta[0] = 0;
-              delta[1] = 0;
-              count = 0;
-            }
-          },
+      body: GestureDetector(
+        onTap: () {
+          ss.socket.emit('click');
+          print("boop");
+        },
+        onPanUpdate: (details) {
+          /*print(
+              details.delta.dx.toString() + " " + details.delta.dy.toString());
+          */
+          if (this.count < val) {
+            delta[0] += details.delta.dx;
+            delta[1] += details.delta.dy;
+            count++;
+          }
+          if (count == val) {
+            ss.socket.emit('cursor', [delta[0], delta[1]]);
+            delta[0] = 0;
+            delta[1] = 0;
+            count = 0;
+          }
+        },
+        child: Container(
+          color: Colors.blueGrey[900],
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            alignment: Alignment.center,
+            clipBehavior: Clip.none,
+            children: [
+              Positioned(
+                top: 40, //MediaQuery.of(context).size.height - 80,
+                child: InkWell(
+                  onTap: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return Popup(ss, (_ip) {
+                            setState(() {
+                              ip = _ip;
+                            });
+                          });
+                        });
+                  },
+                  child: Text(
+                    "Remote",
+                    style: TextStyle(color: Colors.white, fontSize: 32),
+                  ),
+                ),
+              ),
+              Icon(
+                Icons.add,
+                color: Colors.white,
+                size: 48,
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void initSP() async {
+    prefs = await SharedPreferences.getInstance();
+    ip = prefs.getString("ip");
+    ss.createSocketConnection(ip);
+  }
+
+  @override
+  void dispose() {
+    vbe?.cancel();
+    super.dispose();
+  }
+
+  static const volumeChannel = const MethodChannel('volume');
+
+  void listenToVolume() async {
+    try {
+      volumeChannel.setMethodCallHandler(
+          (_didRecieveTranscript)); //volumeChannel.invokeMethod('getVolumeStatus');
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future<void> _didRecieveTranscript(MethodCall call) async {
+    switch (call.method) {
+      case "volume":
+        ss.socket.emit("volume", call.arguments);
+        print(call.arguments);
+    }
   }
 }
